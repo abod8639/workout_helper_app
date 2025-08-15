c8f49c195ff59aa9bb743be84f2679bc1282f6bc
abot8639 <abot8639@gmail.com>
2025-08-15 11:37:21 +0300

    improve TaskService

--- lib/model/jsonModel.dart
+++ lib/model/jsonModel.dart
@@ -1,31 +1,82 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+// class TimeModel {
+//   int? hour;
+//   int? minute;
 
+//   TimeModel({this.hour, this.minute});
 
-class TimeModel{
+//   TimeModel.fromJson(Map<String, dynamic> json) {
+//     hour   = json['hour'];
+//     minute = json['minute'];
+//   }
+// }
 
- int? hour;
- int? minute;
+// class TaskModel {
+//   String? id;
+//   String? name;
+//   TimeModel? time;
 
-
-  TimeModel({this.hour, this.minute});
-  
-  TimeModel.fromJson(Map<String, dynamic> json) {
-    hour = json['hour'];
-    minute = json['minute'];
-  }
-
-}
+//   TaskModel({this.id, this.name, this.time});
+//   TaskModel.fromJson(Map<String, dynamic> json) {
+//     id   = json['id'];
+//     name = json['name'];
+//     time = json['time'] != null ? TimeModel.fromJson(json['time']) : null;
+//   }
+// }
 
 class TaskModel {
+  final String name;
+  final TaskTime time;
 
-  String? id;
-  TimeModel? time;
+  TaskModel({required this.name, required this.time});
 
-
-  TaskModel({this.id, this.time});
-  TaskModel.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    time = json['time'] != null ? TimeModel.fromJson(json['time']) : null;
+  factory TaskModel.fromJson(Map<String, dynamic> json) {
+    return TaskModel(
+      name: json['name'] ?? '',
+      time: TaskTime.fromJson(json['time'] ?? {}),
+    );
   }
 }
 
+class TaskTime {
+  final int hour;
+  final int minute;
+
+  TaskTime({required this.hour, required this.minute});
+
+  factory TaskTime.fromJson(Map<String, dynamic> json) {
+    return TaskTime(hour: json['hour'] ?? 0, minute: json['minute'] ?? 0);
+  }
+}
+
+class TaskService {
+  final String esp32Url = "http://192.168.1.4/tasks";
+
+  // TaskService({required this.esp32Url});
+
+  Future<bool> sendTasks(List<TaskModel> tasks ) async {
+    try {
+      final jsonData = tasks
+          .map(
+            (e) => {
+              "name": e.name,
+              "time": {"hour": e.time.hour, "minute": e.time.minute},
+            },
+          )
+          .toList();
+
+      final response = await http.post(
+        Uri.parse(esp32Url),
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(jsonData),
+      );
+
+      return response.statusCode == 200;
+    } catch (e) {
+      print("Error: $e");
+      return false;
+    }
+  }
+}
--- lib/view/pages/Homepage/homepage.dart
+++ lib/view/pages/Homepage/homepage.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:workout_helper_app/function/SendTask.dart';
+import 'package:workout_helper_app/model/jsonModel.dart';
 import 'package:workout_helper_app/view/pages/Homepage/widget/taskCard.dart';
 import 'package:workout_helper_app/view/pages/Homepage/widget/taskInput.dart';
 
-
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +14,9 @@ class MyHomePage extends StatelessWidget {
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              const Color(0xFF2C5364),  // Deep blue
-              const Color(0xFF203A43),  // Mid blue
-              const Color(0xFF0F2027),  // Dark blue
+              const Color(0xFF2C5364), // Deep blue
+              const Color(0xFF203A43), // Mid blue
+              const Color(0xFF0F2027), // Dark blue
             ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
@@ -69,19 +69,52 @@ class MyHomePage extends StatelessWidget {
                       title: "Push-ups",
                       imageUrl:
                           "https://i.pinimg.com/1200x/7d/ab/36/7dab366ba4ec4013637117176b94ebed.jpg",
-                      onTap: () {},
+                      onTap: () {
+                        TaskService().sendTasks(
+                           [
+                            TaskModel(
+                              name: "Push-ups",
+                              time: TaskTime(
+                                hour: 2,
+                                minute: 50
+                                )),
+                          ],
+                        );
+                      },
                     ),
                     taskCard(
                       title: "Squats",
                       imageUrl:
                           "https://i.pinimg.com/736x/ea/fb/f0/eafbf0d366919fe111f07a6953850ef4.jpg",
-                      onTap: () {},
+                      onTap: () {
+                        TaskService().sendTasks(
+                           [
+                            TaskModel(
+                              name: "Squats",
+                              time: TaskTime(
+                                hour: 4,
+                                minute: 20
+                                )),
+                          ],
+                        );
+                      },
                     ),
                     taskCard(
                       title: "Planks",
                       imageUrl:
                           "https://i.pinimg.com/1200x/3a/c6/45/3ac64594cc1e62fdae241994f698939d.jpg",
-                      onTap: () {},
+                      onTap: () {
+                        TaskService().sendTasks(
+                           [
+                            TaskModel(
+                              name: "Planks",
+                              time: TaskTime(
+                                hour: 10, 
+                                minute: 1
+                                )),
+                          ],
+                        );
+                      },
                     ),
                     taskCard(
                       title: "Burpees",
@@ -98,7 +131,6 @@ class MyHomePage extends StatelessWidget {
                   ],
                 ),
               ),
-
             ],
           ),
         ),
@@ -107,3 +139,7 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+List<TaskModel> pushUps = [
+  TaskModel(name: "Push-ups", time: TaskTime(hour: 1, minute: 30)),
+
+];
