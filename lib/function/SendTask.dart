dd04c4fcb6e08778a3a74850eb26273b1aac3889
abot8639 <abot8639@gmail.com>
2025-08-25 23:30:09 +0300

    format sendTask

--- lib/function/SendTask.dart
+++ lib/function/SendTask.dart
@@ -1,4 +1,4 @@
-  import 'dart:convert';
+import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
@@ -6,26 +6,31 @@ import 'package:workout_helper_app/controller/esp32_controller.Getx.dart';
 import 'package:workout_helper_app/model/jsonModel.dart';
 
 Future<bool> sendTask(List<TaskModel> tasks) async {
-              final controller = Get.find<ESP32Controller>();
+  final controller = Get.find<ESP32Controller>();
 
-        try {
-      final jsonData = tasks
-          .map(
-            (e) => {
-              "name": e.name,
-              "time": {"hour": e.time.hour, "minute": e.time.minute, "test": e.time.test},
+  try {
+    final jsonData = tasks
+        .map(
+          (e) => {
+            "name": e.name,
+            "time": {
+              "hour": e.time.hour,
+              "minute": e.time.minute,
+              "test": e.time.test,
             },
-              ).toList();
+          },
+        )
+        .toList();
 
-      final response = await http.post(
-       Uri.parse('http://${controller.fullIP}/tasks'),
-        headers: {"Content-Type": "application/json"},
-        body: json.encode(jsonData),
-      );
+    final response = await http.post(
+      Uri.parse('http://${controller.fullIP}/tasks'),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(jsonData),
+    );
 
-      return response.statusCode == 200;
-    } catch (e) {
-      print("Error: $e");
-      return false;
-    }
+    return response.statusCode == 200;
+  } catch (e) {
+    print("Error: $e");
+    return false;
   }
+}
